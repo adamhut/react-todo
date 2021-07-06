5a63676645a0472fbdb9e090b4f4a162799923eb
adamhut <adamhut@gmail.com>
2021-07-06 08:57:27 -0700

    wip ep 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,31 @@
+import { useState } from 'react';
+
 import '../reset.css';
 import '../App.css';
 
 function App() {
+  const [todos, setTodos]  = useState([
+    {
+      id:1,
+      title: 'Finish React Series',
+      isComplete:true,
+    },
+    {
+      id:2,
+      title: 'Go Grocery',
+      isComplete:false,
+    },
+    {
+      id:3,
+      title: 'Take Over World',
+      isComplete:false,
+    }
+  ]);
+
+  let changeState= (id)=>{
+    console.log(id);
+  };
+
   return (
     <div className="todo-app-container">
       <div className="todo-app">
@@ -15,6 +39,36 @@ function App() {
         </form>
 
         <ul className="todo-list">
+          {
+             todos.map((todo,index) => {
+                return (
+                  <li className="todo-item-container" key={index}>
+                    <div className="todo-item ">
+                      <input type="checkbox" checked={todo.isComplete} onChange={changeState(todo.id)} />
+                      <span className={`todo-item-label  ${todo.isComplete ? 'line-through':''}` }>{todo.title}</span>
+                      {/* <input type="text" className="todo-item-input" value="Finish React Series" /> */}
+                    </div>
+                    <button className="x-button">
+                      <svg
+                        className="x-button-icon"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M6 18L18 6M6 6l12 12"
+                        />
+                      </svg>
+                    </button>
+                  </li>
+                )
+              })
+          }
+
+
           <li className="todo-item-container">
             <div className="todo-item">
               <input type="checkbox" />
