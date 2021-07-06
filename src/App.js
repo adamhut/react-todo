e91fc9c399b09a4840b7e79934c9d5a3d926b729
adamhut <adamhut@gmail.com>
2021-07-05 20:34:28 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+import { useState } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
+import Another from './another';
+
+
 function App() {
+
+  const [count,setCount]  = useState(0);
+
+  const decrement = ()=>{
+    setCount(prevCount => prevCount-1);
+  };
+  const increment = ()=>{
+    setCount(prevCount => prevCount+1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
+      
+      <header className="App-header bg">
+        <div className="flex items-center">
+          <span>{count}</span>
+          <Another></Another>
+          <button className="bg-gray-100 h-auto text-gray-700  px-4 py-1 rounded shadow-md ml-2" onClick={decrement}>-</button>
+          <button className="bg-gray-100 text-gray-700  px-4 py-1 rounded shadow-md ml-2" onClick={increment}>+</button>
+
+        </div>
+
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="">
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
